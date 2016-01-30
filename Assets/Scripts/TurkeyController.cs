3a0b5db9300ee390938cc6e2105c183d882bab1d
Anil Unnikrishnan <unnikrishnan.anil@gmail.com>
2016-01-30 02:03:33 -0500

    Initial Commit with the map

---
+++ Assets/Scripts/TurkeyController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurkeyController : MonoBehaviour {
+
+	public GameObject lightSource;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Movement
+		transform.Translate(Vector3.forward * 0.01f);
+		
+		float value = Input.GetAxis("Horizontal") * 5;
+		transform.Rotate(0, value, 0);
+	}
+
+	void OnCollisionEnter (Collision col) {
+		string[] name = col.gameObject.name.Split(' ');
+		Debug.Log("i " + name[0] + "j " + name[1]);
+
+		lightSource.transform.position = new Vector3(-0.48f + float.Parse(name[0]),
+		                                             5.76f,
+		                                             0.15f + float.Parse(name[1]));
+	}
+}
